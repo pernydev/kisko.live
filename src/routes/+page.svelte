8c09210f16e955993c45a8d50b821fd2e54fa897
Perny <pernydev@gmail.com>
2024-05-21 10:14:03 +0300

    Renamed project to kisko.live

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <header>
-	<h1 class="mb-2 text-5xl font-bold">Juliet</h1>
+	<h1 class="mb-2 text-5xl font-bold">kisko.live</h1>
 	<p class="mb-6 text-lg">Ilmainen ja avoin junaliikenteen havaintojärjestelmä</p>
 	<button
 		class="flex h-10 w-full max-w-[28rem] rounded-md border border-input bg-background px-3 py-2 text-sm text-muted-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
